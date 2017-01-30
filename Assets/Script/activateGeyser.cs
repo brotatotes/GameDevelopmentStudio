a983afd59bd940aaf2b556ab401dd007eaadbe73
DrDoak <larryzhao740@yahoo.com>
2017-01-29 19:48:47 -0600

    Basic SideScroller framework imported from another Unity project I was working on

---
+++ Assets/Script/activateGeyser.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateGeyser : MonoBehaviour {
+
+	public string collideThisPlayer = "Player 1";
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	internal void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("Geyser");
+		if (other.gameObject.GetComponent<Player>().playerName == "Player1") {
+			gameObject.GetComponent<Collider2D>().isTrigger = true;
+		}
+		else {
+			gameObject.GetComponent<Collider2D>().isTrigger = false;
+		}
+
+		// ActivateGeyser();
+	}
+
+}
---
+++ Assets/Script/deathZone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathZone : MonoBehaviour {
+
+	internal void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("trigger");
+	}
+}
