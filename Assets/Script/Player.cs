c909a0f3ec3fcf977523a03e8abaf7487bda8194
DrDoak <larryzhao740@yahoo.com>
2017-01-31 17:11:07 -0600

    Changed physics

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,43 +4,69 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
-	public float jumpHeight = 4;
+	public float jumpHeight = 4.0f;
 	public float timeToJumpApex = .4f;
 	public string playerName = "Player 1";
 	float accelerationTimeAirborne = .2f;
 	float accelerationTimeGrounded = .1f;
-	float moveSpeed = 6;
+	float moveSpeed = 6.0f;
 
 	float gravity;
 	float jumpVelocity;
 	Vector3 velocity;
 	float velocityXSmoothing;
+	public string leftKey = "a";
+	public string rightKey = "d";
+	public string upKey = "w";
+	public string downKey = "s";
+	public string jumpKey = "space";
 
+	public bool attemptingInteraction = false;
 	Controller2D controller;
 
 	void Start() {
 		controller = GetComponent<Controller2D> ();
-
-		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+		float grav = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		gravity = grav;
+		controller.setGravityScale(grav);
+		jumpVelocity = Mathf.Abs(grav) * timeToJumpApex;
+		print ("Gravity: " + grav + "  Jump Velocity: " + jumpVelocity);
 	}
 
 	void Update() {
 
 		if (controller.collisions.above || controller.collisions.below) {
-			velocity.y = 0;
+			velocity.y = 0.0f;
 		}
 
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		float inputX = 0.0f;
+		if (Input.GetKey(leftKey)) {
+			inputX = -1.0f;
+		} else if (Input.GetKey(rightKey)) {
+			inputX = 1.0f;
+		}
+		float inputY = 0.0f;
+		if (Input.GetKey(upKey)) {
+			inputY = 1.0f;
+		} else if (Input.GetKey(downKey) ){
+			inputY = -1.0f;
+		}
+		if (Input.GetKeyDown (downKey)) {
+			attemptingInteraction = true;
+		} else {
+			attemptingInteraction = false;
+		}
 
-		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+				
+		if (Input.GetKey (jumpKey) && controller.collisions.below) {
 			velocity.y = jumpVelocity;
 		}
-
-		float targetVelocityX = input.x * moveSpeed;
+		float targetVelocityX = inputX * moveSpeed;
 		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		Vector2 input = new Vector2 (inputX, inputY);
 		velocity.y += gravity * Time.deltaTime;
-		controller.Move (velocity * Time.deltaTime, input);
+		//controller.Move (velocity * Time.deltaTime, input);
+		//Debug.Log(velocity.y);
+		controller.Move (velocity, input);
 	}
 }
