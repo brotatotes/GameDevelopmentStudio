846760fda3fa094d06fb83dadd4f443acdfbd3c1
LuoLei_Zhao <luoleizhao2018@u.northwestern.org>
2017-01-31 18:50:20 -0600

    Added a hitbox as well as a bomb object that creates a hitbox

---
+++ Assets/Script/bomb.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bomb : MonoBehaviour {
+
+	public GameObject hitboxClass;
+	public float damage = 10.0f;
+	public Vector2 knockback = new Vector2(0.0f,40.0f);
+	public float fuseDuration = 3.0f;
+	public bool timedbomb = true;
+
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fuseDuration > 0.0f && timedbomb) {
+			fuseDuration = fuseDuration - Time.deltaTime;
+		} else {
+			Debug.Log ("Explosion");
+			GameObject go = Instantiate(hitboxClass,transform.position,Quaternion.identity) as GameObject; 
+			hitbox newBox = go.GetComponent<hitbox> ();
+			newBox.setScale (new Vector2 (4.0f, 4.0f));
+			newBox.setDamage (40.0f);
+			newBox.setHitboxDuration (0.5f);
+
+			GameObject.Destroy (gameObject);
+		}
+	}
+	void setDamage(float dmg) {
+		damage = dmg;
+	}
+	void setFuseDuration (float time) {
+		fuseDuration = time;
+	}
+}
---
+++ Assets/Script/hitbox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitbox : MonoBehaviour {
+
+	public float damage = 10.0f;
+	public Vector2 knockback = new Vector2(0.0f,40.0f);
+	public float hitboxDuration = 1.0f;
+	public bool timedHitbox = true;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("explosion created");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hitboxDuration > 0.0f && timedHitbox) {
+			hitboxDuration = hitboxDuration - Time.deltaTime;
+		} else {
+			GameObject.Destroy (gameObject);
+		}
+	}
+
+	public void setDamage(float dmg) {
+		damage = dmg;
+	}
+	public void setHitboxDuration (float time) {
+		hitboxDuration = time;
+	}
+	public void setScale(Vector2 scale) {
+		transform.localScale = scale;
+	}
+	internal void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("Ontriggerenter");
+		if (other.gameObject.GetComponent<Controller2D>()) {
+			Debug.Log ("correct collision");
+			Controller2D otherObj = other.gameObject.GetComponent<Controller2D> ();
+			otherObj.damageObj (damage);
+			otherObj.Move (knockback);
+		}
+	}
+}
