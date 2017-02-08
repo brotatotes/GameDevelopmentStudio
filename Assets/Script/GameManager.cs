e70c77f4185d0be7ae817b9a489a00f3d683b1da
brotatotes <hao.awesome@gmail.com>
2017-02-07 19:02:10 -0600

    Setup GameManager essentials, including Loader class

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager instance = null;
 	public int winner = 0; // 0 for no winner, 1 for player 1, 2 for player 2
 	public bool gameOver = false;
+	public Player Player1;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
 		winner = 0;
 		gameOver = false;
+		InitGame ();
 	}
-	
+
+	void InitGame() {
+		
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
