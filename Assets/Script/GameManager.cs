77a26d18b88d813241890d84052bf65c22acaa10
brotatotes <hao.awesome@gmail.com>
2017-02-07 18:38:08 -0600

    GUIHandler class and GameManager class.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int winner = 0; // 0 for no winner, 1 for player 1, 2 for player 2
+	public bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		winner = 0;
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
