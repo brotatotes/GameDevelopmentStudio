cf60f728dd6aac81a5018e71073ef663e3b325b4
LuoLei_Zhao <luoleizhao2018@u.northwestern.org>
2017-02-12 18:05:16 -0600

    Adjusted hitbox explosion will now counter vertical velcoity

--- Assets/Script/bomb.cs
+++ Assets/Script/bomb.cs
@@ -14,7 +14,7 @@ public class bomb : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (fuseDuration > 0.0f && timedbomb) {
