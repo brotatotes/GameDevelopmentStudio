51f9fef090482574563400a098370697d7e25c70
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-03-01 13:15:52 -0600

    Fixed weird camera motions with background`

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -24,7 +24,7 @@ public class CameraFollow : MonoBehaviour {
 		focusArea = new FocusArea (target.GetComponent<Collider2D>().bounds, focusAreaSize);
 	}
 
-	void LateUpdate() {
+	void Update() {
 		focusArea.Update (target.GetComponent<Collider2D>().bounds);
 
 		Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
