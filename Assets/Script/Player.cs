6762aa5a69773e9bd2de728e3e4e845bee837da4
brotatotes <hao.awesome@gmail.com>
2017-03-01 14:15:26 -0600

    adjust player spawn loaction

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -60,9 +60,9 @@ public class Player : MonoBehaviour {
 
 	public void Reset() {
 		if (spawnNextToEndzone)
-			startPosition = new Vector2 (105f, 14f); // this is right next to the endzone.
+			startPosition = new Vector2 (105f, 14f); // this is right next to the endzone. (in Scene.unity)
 		else
-			startPosition = new Vector2 (-4.0f, 1.5f);
+			startPosition = new Vector2 (-4.0f, -4f);
 		transform.position = startPosition;
 		controller.accumulatedVelocity = Vector2.zero;
 		attackable.resetHealth ();
